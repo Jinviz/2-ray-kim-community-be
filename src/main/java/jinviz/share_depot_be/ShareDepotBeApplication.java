aa3afd57d8675e5378d6e4a4af613cbed34f5a96
Jinviz <rlawlsdn9583@gmail.com>
2025-03-20 13:13:01 +0900

    Initialize projects using spring-initializr

---
+++ src/main/java/jinviz/share_depot_be/ShareDepotBeApplication.java
@@ -0,0 +1,13 @@
+package jinviz.share_depot_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareDepotBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShareDepotBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jinviz/share_depot_be/ShareDepotBeApplicationTests.java
@@ -0,0 +1,13 @@
+package jinviz.share_depot_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareDepotBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
